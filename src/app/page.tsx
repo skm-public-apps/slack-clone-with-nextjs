05826f02420d88c3e7af27142b46dc3a78d889c8
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2025-07-11 07:43:47 -0500

    user-button-component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
+import UserButton from "@/features/auth/components/user-button";
 export default function Home() {
-  const { signOut } = useAuthActions();
   return (
     <div className="">
-      Logged in
-      <Button onClick={() => signOut()}>Signout</Button>
+      <UserButton />
     </div>
   );
 }
