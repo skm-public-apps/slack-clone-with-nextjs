eb28022d5a7753d3e26e9d3a4b0cb1849b0c0cb0
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2025-07-10 20:50:29 -0500

    google-authentication

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,5 +1,6 @@
 import GitHub from "@auth/core/providers/github";
+import Google from "@auth/core/providers/google";
 import { convexAuth } from "@convex-dev/auth/server";
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [GitHub],
+  providers: [GitHub, Google],
 });
