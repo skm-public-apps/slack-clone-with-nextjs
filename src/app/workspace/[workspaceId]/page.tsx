e4939bdf6bc057a4e6e5371ce86bd6a02cfb41fa
Santhosh Kumar Muddamsetty <muddamsetty.santhosh@gmail.com>
2025-07-16 07:51:31 -0500

    sidebar-component

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,10 +1,3 @@
-"use client";
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-
 export default function WorkspaceIdPage() {
-  const workspaceId = useWorkspaceId();
-  const { data, isLoading } = useGetWorkspace({ id: workspaceId });
-  console.log(JSON.stringify(data));
-  return <div>WorkspaceIdPage {workspaceId}</div>;
+  return <div>WorkspaceIdPage </div>;
 }
